09f99a98f07d211984c1abf03b4eb3921965ab38
Alex Basystyi <alexbasisty@gmail.com>
2020-04-22 11:55:00 +0200

    redux store

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -4,16 +4,21 @@ import App from "./App";
 import AdminPanel from "./AdminPanel";
 import PageNotFound from "./PageNotFound";
 
+import Provider from "react-redux";
+import store from "../store/store";
+
 class Router extends Component {
     render() {
         return (
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path="/" component={App}></Route>
-                    <Route path="/admin" component={AdminPanel}></Route>
-                    <Route component={PageNotFound} />
-                </Switch>
-            </BrowserRouter>
+            <Provider store={store}>
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path="/" component={App}></Route>
+                        <Route path="/admin" component={AdminPanel}></Route>
+                        <Route component={PageNotFound} />
+                    </Switch>
+                </BrowserRouter>
+            </Provider>
         );
     }
 }
